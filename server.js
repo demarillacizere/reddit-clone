53f76dfd67d9a72a93e789517ab394301531584a
demizere <l.izere@alustudent.com>
2022-05-05 10:35:55 +0200

    deployment

---
+++ server.js
@@ -0,0 +1,15 @@
+// create an express app
+const express = require("express")
+const app = express()
+
+// use the express-static middleware
+app.use(express.static("public"))
+
+// define the first route
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
+
+// listen for requests 
+app.listen(process.env.PORT || 3001, 
+    () => console.log("Server is running..."));
